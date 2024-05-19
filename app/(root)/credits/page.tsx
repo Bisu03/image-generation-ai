986903cbd6210f3b3ddd4f5759ccc6f1818945cb
BISWANATH BERA <berabiswanath338@gmail.com>
2024-05-19 19:57:13 +0530

    first commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+const Credit = () => {
+  return (
+    <div>
+      Credit
+      <UserButton />
+    </div>
+  );
+};
+
+export default Credit;
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddtransformationsType = () => {
+  return (
+    <div>AddtransformationsType</div>
+  )
+}
+
+export default AddtransformationsType
